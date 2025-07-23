886311c911e04151edabdce6147542dff0f72bab
Pablo Ramírez <pabloram.dev@gmail.com>
2025-07-22 21:32:12 -0400

    animate hero section

--- src/_components/_hero/hero.tsx
+++ src/_components/_hero/hero.tsx
@@ -6,12 +6,52 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
 export default function Hero() {
+  // const timeline = gsap.timeline();
 
   useGSAP(() => {
+
+
     gsap.from("#hero-phone", {
       y: 250,
       duration: 1,
-      ease: 'power1.in'
+      opacity:0,
+      scale: 1.1,
+      ease: 'power0.in'
+    })
+
+    gsap.from("#hero-pig", {
+      y:100,
+      duration: 1,
+      scale: 1.1,
+      opacity: 0,
+      ease: "power0.in"
+    })
+    gsap.from("#hero-moneybag", {
+      x: 10,
+      y:-100,
+      scale: 1.1,
+      opacity: 0,
+      duration: 1,
+      ease: "power0.in"
+    })
+    gsap.from("#hero-h1", {
+      y:-10,
+      duration: 1,
+      scale: 1.1,
+      ease: "power0.in"
+    })
+    gsap.from("#hero-p", {
+      y:10,
+      duration: 1,
+      scale: 1.1,
+      ease: "power0.in"
+    })
+
+    gsap.from("#hero-actions", {
+      y: 20,
+      duration: 0.5,
+      opacity: 0,
+      delay: 0.5
     })
   }, [])
   return (
@@ -25,14 +65,14 @@ export default function Hero() {
         className="absolute top-0 left-0"
       />
       <div className="relative flex flex-col items-center justify-center gap-xs top-3xl">
-        <h1 className="font-open-sans font-bold text-xl justify-center items-center">
+        <h1 id="hero-h1" className="font-open-sans font-bold text-xl justify-center items-center">
           Una sola cuenta, sin cuentos
         </h1>
-        <p>
+        <p id="hero-p">
           Compra, paga, ahorra y envía dinero sin cobros. Todo claro, sin
           cuentos.
         </p>
-        <div className="flex gap-xs w-3/4">
+        <div id="hero-actions" className="flex gap-xs w-3/4">
           <CtaButton text="Comienza Ya!" isPrimary={true} />
           <CtaButton text="Pedir tarjeta física" isPrimary={false} />
         </div>
@@ -49,8 +89,8 @@ export default function Hero() {
         id="hero-pig"
         src={"/images/pig.png"}
         alt="pig 3d emoji"
-        width={402}
-        height={402}
+        width={452}
+        height={452}
         className="absolute bottom-0 right-[10%]"
       />
     </section>
