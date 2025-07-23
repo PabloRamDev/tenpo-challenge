4bf7350b24bd686b903d6477cbd4361f8048e83d
Pablo Ramírez <pabloram.dev@gmail.com>
2025-07-22 20:48:15 -0400

    add cta-button component

--- src/_components/_ui/_buttons/cta-button.tsx
+++ src/_components/_ui/_buttons/cta-button.tsx
@@ -3,10 +3,10 @@ import clsx from 'clsx'
 
 type ctaButtonProps = {
     text: string,
-    variant: string,
+    isPrimary: boolean,
 }
-export default function CtaButton() {
+export default function CtaButton({text, isPrimary} : ctaButtonProps) {
   return (
-    <div>cta-button</div>
+    <div className={clsx('rounded-md p-md bg-neutral-900 text-text-0', {'bg-primary-400 text-text-900' : isPrimary})}>{text}</div>
   )
 }
