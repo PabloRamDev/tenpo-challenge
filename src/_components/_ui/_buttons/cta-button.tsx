6c37809524b982b69bcd24af5d33a209c2c86225
Pablo Ramírez <pabloram.dev@gmail.com>
2025-07-22 20:43:32 -0400

    add clsx dependency

---
+++ src/_components/_ui/_buttons/cta-button.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import clsx from 'clsx'
+
+type ctaButtonProps = {
+    text: string,
+    variant: string,
+}
+export default function CtaButton() {
+  return (
+    <div>cta-button</div>
+  )
+}
